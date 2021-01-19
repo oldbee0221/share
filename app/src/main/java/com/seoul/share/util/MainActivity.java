8f64261bdffc0323f6df98f021ac02e5f84220f9
Loner123 <dlwlstjq410@gmail.com>
2021-01-19 17:30:37 +0900

    [Modify] 클릭시 icon 변하지 않는 문제 해결, 바텀네비에 네비게이션 적용

--- app/src/main/java/com/seoul/share/util/MainActivity.java
+++ app/src/main/java/com/seoul/share/util/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.seoul.share.R;
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private final Fragment_menu4 fragment_menu4 = new Fragment_menu4();
     private final Fragment_menu5 fragment_menu5 = new Fragment_menu5();
 
-
+    private ActivityMainBinding binding;
     Menu menu;
     ArrayList<Integer> selectDrawableList = new ArrayList<>();
     ArrayList<Integer> notSelectDrawableList = new ArrayList<>();
@@ -37,118 +39,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         menu = binding.navView.getMenu();
+        setNavigation();
 
-        setDrableListData();
-        setBottomNavi(binding);
-    }// onCreate()..
-
-    private void setDrableListData() {
-        notSelectDrawableList.add(R.drawable.home2_);
-        notSelectDrawableList.add(R.drawable.use_);
-        notSelectDrawableList.add(R.drawable.myproducts2_);
-        notSelectDrawableList.add(R.drawable.chat_);
-        notSelectDrawableList.add(R.drawable.townpage_);
-
-        selectDrawableList.add(R.drawable.home_);
-        selectDrawableList.add(R.drawable.use2_);
-        selectDrawableList.add(R.drawable.myproducts_);
-        selectDrawableList.add(R.drawable.chat2_);
-        selectDrawableList.add(R.drawable.townpage2_);
-
-        drawableList.addAll(notSelectDrawableList);
-    }
-    
-    private void setBottomNavi(ActivityMainBinding binding) {
-        binding.navView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
-        getSupportFragmentManager().beginTransaction().replace(R.id.layout_main_frame, fragment_menu1).commitAllowingStateLoss();
-        binding.navView.setSelectedItemId(R.id.navigation_1);  //선택된 아이템 지정
     }
 
 
-    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
-        @SuppressLint("NonConstantResourceId")
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switch (menuItem.getItemId()) {
-                case R.id.navigation_1:
-
-                    setIconList(0);
-                    setFragment(1);
-                    break;
-
-
-                case R.id.navigation_2:
-
-                    setIconList(1);
-                    setFragment(2);
-                    break;
-
-                case R.id.navigation_3:
-                    setIconList(2);
-                    setFragment(3);
-                    break;
-
-                case R.id.navigation_4:
-                    setIconList(3);
-                    setFragment(4);
-                    break;
-
-
-                case R.id.navigation_5:
-                    setIconList(4);
-                    setFragment(5);
-                    break;
-
-
-            }// switch()..
-            return true;
-        }
-
-        private void setIconList(int index) {
-
-            drawableList.set(index, selectDrawableList.get(index));
-
-            for (int i = 0; i < menu.size(); i++) {
-                if (i != index)
-                    drawableList.set(i, notSelectDrawableList.get(i));
-            }
-
-            for (int j = 0; j < menu.size(); j++) {
-                    menu.getItem(j).setIcon(drawableList.get(j));
-            }
-        }
-
-        private void setFragment(int index) {
-            switch (index) {
-                case 1:
-                    getSupportFragmentManager().beginTransaction().
-                            replace(R.id.layout_main_frame, fragment_menu1).commitAllowingStateLoss();
-                    break;
-                case 2:
-                    getSupportFragmentManager().beginTransaction().
-                            replace(R.id.layout_main_frame, fragment_menu2).commitAllowingStateLoss();
-                    break;
-                case 3:
-                    getSupportFragmentManager().beginTransaction().
-                            replace(R.id.layout_main_frame, fragment_menu3).commitAllowingStateLoss();
-                    break;
-                case 4:
-                    getSupportFragmentManager().beginTransaction().
-                            replace(R.id.layout_main_frame, fragment_menu4).commitAllowingStateLoss();
-                    break;
-                case 5:
-                    getSupportFragmentManager().beginTransaction().
-                            replace(R.id.layout_main_frame, fragment_menu5).commitAllowingStateLoss();
-                    break;
-            }
-
+        private void setNavigation() {
+            NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.layout_main_frag);
+            assert navHostFragment != null;
+            NavigationUI.setupWithNavController(binding.navView, navHostFragment.getNavController());
+            binding.navView.setItemIconTintList(null);
 
         }
+    }
 
-    }// ItemSelectedListener class..
 
-
-}
--- app/src/main/java/com/seoul/share/util/TutorialActivity.java
+++ app/src/main/java/com/seoul/share/util/TutorialActivity.java
@@ -10,10 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -27,7 +26,6 @@ public class TutorialActivity extends AppCompatActivity {
 
     public static Object Context;
     private ViewPagerAdapter pagerAdapter;
-    private LinearLayout dotsLayout;
     private TextView[] dots;
     private int[] layouts;
 
@@ -38,12 +36,9 @@ public class TutorialActivity extends AppCompatActivity {
 
         binding = ActivityTutorialBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        if (Build.VERSION.SDK_INT >= 21) {
+        if (Build.VERSION.SDK_INT >= 23) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
-
-
-
 
 
 // 변화될 레이아웃들 주소
@@ -89,12 +84,12 @@ public class TutorialActivity extends AppCompatActivity {
         int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
         int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
 
-        dotsLayout.removeAllViews();
+        binding.layoutDots.removeAllViews();
         for (int i = 0; i < dots.length; i++) {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#8226;"));
             dots[i].setTextSize(35);
-            dotsLayout.addView(dots[i]);
+            binding.layoutDots.addView(dots[i]);
         }
 
         if (dots.length > 0)
@@ -141,15 +136,13 @@ public class TutorialActivity extends AppCompatActivity {
     };
 
     private void changeStatusBarColor() { // 최상단 바 색 변경
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
     }
 
     public class ViewPagerAdapter extends PagerAdapter {
-        private LayoutInflater mInflater;
+        private final LayoutInflater mInflater;
 
         public ViewPagerAdapter(LayoutInflater inflater) {
             this.mInflater = inflater;
@@ -161,12 +154,13 @@ public class TutorialActivity extends AppCompatActivity {
         }
 
         @Override
-        public boolean isViewFromObject(View view, Object object) {
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
             return view == object;
         }
 
+        @NonNull
         @Override
-        public Object instantiateItem(ViewGroup container, int position) {
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
 
             View view = mInflater.inflate(layouts[position], container, false);
             container.addView(view);
